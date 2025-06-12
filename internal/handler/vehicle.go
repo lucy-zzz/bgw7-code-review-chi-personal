1c01de9217b1f83498b5a012fe5a885a9f595e4b
Lucyla De Souza Lima Loro <llloro@br000m2rxnwj1wm.ml.com>
2025-06-12 10:36:02 -0300

    feat: first exc

---
+++ internal/handler/vehicle.go
@@ -0,0 +1,120 @@
+package handler
+
+import (
+	"app/internal"
+	"encoding/json"
+	"net/http"
+
+	"github.com/bootcamp-go/web/response"
+)
+
+// VehicleJSON is a struct that represents a vehicle in JSON format
+type VehicleJSON struct {
+	ID              int     `json:"id"`
+	Brand           string  `json:"brand"`
+	Model           string  `json:"model"`
+	Registration    string  `json:"registration"`
+	Color           string  `json:"color"`
+	FabricationYear int     `json:"year"`
+	Capacity        int     `json:"passengers"`
+	MaxSpeed        float64 `json:"max_speed"`
+	FuelType        string  `json:"fuel_type"`
+	Transmission    string  `json:"transmission"`
+	Weight          float64 `json:"weight"`
+	Height          float64 `json:"height"`
+	Length          float64 `json:"length"`
+	Width           float64 `json:"width"`
+}
+
+// NewVehicleDefault is a function that returns a new instance of VehicleDefault
+func NewVehicleDefault(sv internal.VehicleService) *VehicleDefault {
+	return &VehicleDefault{sv: sv}
+}
+
+// VehicleDefault is a struct with methods that represent handlers for vehicles
+type VehicleDefault struct {
+	// sv is the service that will be used by the handler
+	sv internal.VehicleService
+}
+
+// GetAll is a method that returns a handler for the route GET /vehicles
+func (h *VehicleDefault) GetAll() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// request
+		// ...
+
+		// process
+		// - get all vehicles
+		v, err := h.sv.FindAll()
+		if err != nil {
+			response.JSON(w, http.StatusInternalServerError, nil)
+			return
+		}
+
+		// response
+		data := make(map[int]VehicleJSON)
+		for key, value := range v {
+			data[key] = VehicleJSON{
+				ID:              value.Id,
+				Brand:           value.Brand,
+				Model:           value.Model,
+				Registration:    value.Registration,
+				Color:           value.Color,
+				FabricationYear: value.FabricationYear,
+				Capacity:        value.Capacity,
+				MaxSpeed:        value.MaxSpeed,
+				FuelType:        value.FuelType,
+				Transmission:    value.Transmission,
+				Weight:          value.Weight,
+				Height:          value.Height,
+				Length:          value.Length,
+				Width:           value.Width,
+			}
+		}
+		response.JSON(w, http.StatusOK, map[string]any{
+			"message": "success",
+			"data":    data,
+		})
+	}
+}
+
+func (h *VehicleDefault) Create() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var input internal.VehicleAttributes
+
+		err := json.NewDecoder(r.Body).Decode(&input)
+		if err != nil {
+			w.Write([]byte(`{message: 400 Bad Request: Dados do veículo mal formatados ou incompletos.}`))
+			response.JSON(w, http.StatusBadRequest, 400)
+			return
+		}
+
+		newVehicleAttributes := internal.VehicleAttributes{
+			Brand:           input.Brand,
+			Model:           input.Model,
+			Registration:    input.Registration,
+			Color:           input.Color,
+			FabricationYear: input.FabricationYear,
+			Capacity:        input.Capacity,
+			MaxSpeed:        input.MaxSpeed,
+			FuelType:        input.FuelType,
+			Transmission:    input.Transmission,
+			Weight:          input.Weight,
+			Dimensions:      input.Dimensions,
+		}
+
+		id, err := h.sv.Create(newVehicleAttributes)
+		if err != nil {
+			w.Write([]byte(`{message: 409 Conflict: Identificador do veículo já existente.}`))
+			response.JSON(w, http.StatusBadRequest, 400)
+			return
+		}
+
+		response.JSON(w, http.StatusOK, map[string]any{
+			"message": "success",
+			"data":    id,
+		})
+
+	}
+
+}
---
+++ internal/service/vehicle_default.go
@@ -0,0 +1,44 @@
+package service
+
+import "app/internal"
+
+// NewVehicleDefault is a function that returns a new instance of VehicleDefault
+func NewVehicleDefault(rp internal.VehicleRepository) *VehicleDefault {
+	return &VehicleDefault{rp: rp}
+}
+
+// VehicleDefault is a struct that represents the default service for vehicles
+type VehicleDefault struct {
+	// rp is the repository that will be used by the service
+	rp internal.VehicleRepository
+}
+
+// FindAll is a method that returns a map of all vehicles
+func (s *VehicleDefault) FindAll() (v map[int]internal.Vehicle, err error) {
+	v, err = s.rp.FindAll()
+	return
+}
+
+func (s *VehicleDefault) Create(new internal.VehicleAttributes) (v internal.Vehicle, err error) {
+	all, err := s.rp.FindAll()
+
+	if err != nil {
+		return internal.Vehicle{}, err
+	}
+
+	v = internal.Vehicle{
+		Id:                getNextID(all),
+		VehicleAttributes: new,
+	}
+	return v, nil
+}
+
+func getNextID(vehicles map[int]internal.Vehicle) int {
+	maxID := 0
+	for _, v := range vehicles {
+		if v.Id > maxID {
+			maxID = v.Id
+		}
+	}
+	return maxID + 1
+}
